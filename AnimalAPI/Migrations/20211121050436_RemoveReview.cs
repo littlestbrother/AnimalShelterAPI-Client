1ab2f046a6a9f74ff5140162e1c38795de7f7823
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-11-20 21:42:16 -0800

    add README.md

---
+++ AnimalAPI/Migrations/20211121050436_RemoveReview.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalAPI.Migrations
+{
+    public partial class RemoveReview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Family = table.Column<string>(type: "varchar(100) CHARACTER SET utf8mb4", maxLength: 100, nullable: false),
+                    Age = table.Column<string>(type: "varchar(999) CHARACTER SET utf8mb4", maxLength: 999, nullable: false),
+                    Sex = table.Column<string>(type: "varchar(6) CHARACTER SET utf8mb4", maxLength: 6, nullable: false),
+                    Admission = table.Column<string>(type: "varchar(10) CHARACTER SET utf8mb4", maxLength: 10, nullable: false),
+                    Breed = table.Column<string>(type: "varchar(100) CHARACTER SET utf8mb4", maxLength: 100, nullable: false),
+                    UserName = table.Column<string>(type: "varchar(10) CHARACTER SET utf8mb4", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,63 @@
+# Animal Shelter
+
+### ***A C# API with a web-pack front end that handles full CRUD functionality for an Animal Shelter***
+
+### **By *Aaron Kauffman***
+
+## **Description**
+
+*This web application helps a hypothetical Animal Shelter manage what animals are in the shelter.* 
+
+# Instructions
+
+### API Setup:
+
+1. Clone the repository.
+2. Add the file `appsettings.json` to the `AnimalAPI` folder. ***It's contents also include other MVC files and folders.***
+3. Here's what `appsettings.json` should contain for this specific project. Replace your [DATABASE] and [PASSWORD] appropriately.
+
+```json
+{
+  "ConnectionStrings": {
+      "DefaultConnection": "Server=localhost;Port=3306;database=[DATABASE];uid=root;pwd=[PASSWORD];"
+  }
+}
+```
+
+1. Create a schema in MySQL Workbench- the name of the database ***must*** match the database name from your `appsettings.json`.
+2. From within the `AnimalAPI` directory; build the database with:
+
+```bash
+dotnet ef database update
+```
+
+1. From the uppermost directory of your project run:
+
+```bash
+cd AnimalAPI && dotnet run
+```
+
+### Client Setup:
+
+Navigate to the `AnimalClient` directory and from within run:
+
+```bash
+npm i && npm run start
+```
+
+## **Technologies Used**
+
+- C#, MVC, [Asp.Net](http://asp.net/) Core, EF Core, MySQL.
+- HTML, JS, CSS, Bootstrap, git, npm, webpack, jquery
+
+## **Known Bugs**
+
+- None found so far. Go ahead and break it i guess.
+
+## **License**
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## **Contact Information**
+
+*If you run into any issues, remember: Stop, Drop, and Roll. Or, Contact Aaron at: [Aaron.Christian.Kauffman@gmail.com](mailto:Aaron.Christian.Kauffman@gmail.com)*
